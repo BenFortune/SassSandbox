a4fe7e600fdfaa498a0de291bde0e2d8feacf441
Ben Fortune <ben.fortune@gmail.com>
2015-05-22 19:37:35 -0700

    Adding scss lint config

--- grunt/scsslint.js
+++ grunt/scsslint.js
@@ -1,4 +1,9 @@
 module.exports = {
+  options: {
+    colorizeOutput: true,
+    compact: true,
+    config: '.scss-lint.yml',
+  },
   main: {
     files: {
       'scss/main.scss': 'scss/main.scss'
