62378cbf371b2846b98f0d47bacb6bf82649b729
Ben Fortune <ben.fortune@gmail.com>
2015-05-24 22:50:35 -0500

    More work on float label

--- js/index.js
+++ js/index.js
@@ -1,40 +1,31 @@
 $(function() {
   'use strict';
-  console.log("function executed");
   $(document).ready(function() {
-    var floatInput = $('.form-group input');
-    var onClass = "float-label";
-    var showClass = "float-label-on";
+    var floatLabel = $('#float-label label');
+    var floatInput = $('#float-label input');
+    var onClass = "float-label-on";
+    var showClass = "float-label";
 
     floatInput.on('focus', function() {
-      focusHandler();
+      floatLabelHandler();
     });
     floatInput.on('blur', function() {
-      blurHandler();
+      floatLabelHandler();
     });
+    floatInput.on('keyup', function() {
+      console.log("this is", this);
+      floatLabelHandler();
+    })
 
-    function focusHandler() {
-      if (this.val() === "" || this.val() === undefined) {
-        console.log("focused - value is nothing");
-        this.removeClass('float-label-on');
-        this.sibling().removeClass('float-label');
+    function floatLabelHandler() {
+      if (!floatInput.val() || floatInput.val() === "") {
+        floatLabel.removeClass(onClass);
+        floatInput.removeClass(showClass);
       } else {
-        console.log("focused - value is something");
-        this.addClass('float-label-on');
-        this.sibling().addClass('float-label');
+        floatLabel.addClass(onClass);
+        floatInput.addClass(showClass);
       }
     }
 
-    function blurHandler() {
-      if (this.val() === "" || this.val() === undefined) {
-        console.log("blurred - value is nothing");
-        this.removeClass('float-label-on');
-        this.sibling().removeClass('float-label');
-      } else {
-        console.log("blurred - value is something");
-        this.addClass('float-label-on');
-        this.sibling().addClass('float-label');
-      }
-    }
   })
 });
