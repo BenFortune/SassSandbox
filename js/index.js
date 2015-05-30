3671da9b7fb4184c3d868676f4a700f4b4323ee1
Ben Fortune <ben.fortune@gmail.com>
2015-05-29 21:37:53 -0500

    Misc work on selects

--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
-$(function() {
-  'use strict';
-  $(document).ready(function() {
-    var floatLabel = $('#float-label label');
-    var floatInput = $('#float-label input');
-    var onClass = "float-label-on";
-    var showClass = "float-label";
+$(document).ready(function() {
 
-    floatInput.on('focus', function() {
-      floatLabelHandler.call(floatLabel, 'something else');
-    });
-    floatInput.on('blur', function() {
-      floatLabelHandler.call(floatLabel, 'something else');
-    });
-    floatInput.on('keyup', function() {
-      floatLabelHandler.call(floatLabel, 'something else');
-    });
+// Float Label
+function floatLabel() {
+  var floatLabel = $('#float-label label');
+  var floatInput = $('#float-label input');
+  var onClass = "float-label-on";
+  var showClass = "float-label";
 
-    function floatLabelHandler() {
-      if (!floatInput.val() || floatInput.val() === "") {
-        this.removeClass(onClass);
-        this.next().removeClass(showClass);
-      } else {
-        this.addClass(onClass);
-        this.next().addClass(showClass);
-      }
+  floatInput.on('focus', function() {
+    floatLabelHandler.call(floatLabel, 'something else');
+  });
+  floatInput.on('blur', function() {
+    floatLabelHandler.call(floatLabel, 'something else');
+  });
+  floatInput.on('keyup', function() {
+    floatLabelHandler.call(floatLabel, 'something else');
+  });
+
+  function floatLabelHandler() {
+    if (!floatInput.val() || floatInput.val() === "") {
+      this.removeClass(onClass);
+      this.next().removeClass(showClass);
+    } else {
+      this.addClass(onClass);
+      this.next().addClass(showClass);
     }
+  }
+}
 
-  })
+// Multi Select and Single Select Dropdown
+function selectDD() {
+  console.log("Select DD Initialized")
+}
+
+// Intiate Functions
+floatLabel();
+selectDD();
 });
