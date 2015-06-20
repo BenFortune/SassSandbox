cd34b21346857c1c1ec8aa964cfa9fd3d642fddd
Ben Fortune <ben.fortune@gmail.com>
2015-06-19 23:24:13 -0700

    Sassdoc work

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,8 +1,12 @@
 module.exports = {
   'default': [],
+  'sassdoc': [
+    'sassdoc:fileOnly'
+  ],
   'main': [
     'csscomb:main',
     'sass:main',
+    'sassdoc:main',
     'concat:main',
     'cssmin:main'
   ]
---
+++ grunt/sassdoc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  options: {
+    display: {
+      access: ['public', 'private'],
+      alias: true,
+      watermark: true,
+    },
+  }
+  fileOnly: {
+    src: 'scss/main.scss'
+  },
+  main: {
+    src: 'scss/**/*.scss'
+  }
+}
