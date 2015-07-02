1695754a6eebf8388c680197cf96f9e14d28b506
Ben Fortune <ben.fortune@gmail.com>
2015-07-01 21:07:02 -0700

    Misc syntax cleanup

--- js/index.js
+++ js/index.js
@@ -1,109 +1,109 @@
 $(document).ready(function() {
 
-var mySlider = $("#slider").slider();
-    mySlider.slider('setValue', 8);
+  var mySlider = $("#slider").slider();
+      mySlider.slider('setValue', 8);
 
-// Float Label
-(function floatLabel() {
-  var floatInput = $('.float-label input');
-  var onClass = "float-label-on";
-  var showClass = "float-label";
+  // Float Label
+  (function floatLabel() {
+    var floatInput = $('.float-label input');
+    var onClass = "float-label-on";
+    var showClass = "float-label";
 
-  floatInput.on('focus', function(e) {
-    floatLabelHandler.call(e.target, 'something else');
-  });
-  floatInput.on('blur', function(e) {
-    floatLabelHandler.call(e.target, 'something else');
-  });
-  floatInput.on('keyup', function(e) {
-    floatLabelHandler.call(e.target, 'something else');
-  });
+    floatInput.on('focus', function(e) {
+      floatLabelHandler.call(e.target, 'something else');
+    });
+    floatInput.on('blur', function(e) {
+      floatLabelHandler.call(e.target, 'something else');
+    });
+    floatInput.on('keyup', function(e) {
+      floatLabelHandler.call(e.target, 'something else');
+    });
 
-  function floatLabelHandler() {
-    if (!this.value || this.value === "") {
-      $(this).removeClass(showClass);
-      $(this).prev().removeClass(onClass);
-    } else {
-      $(this).addClass(showClass);
-      $(this).prev().addClass(onClass);
-    }
-  }
-})();
-
-// Multi Select and Single Select Dropdown
-(function selectDD() {
-  var toggleTrigger = $('.dropdown-toggle');
-
-  toggleTrigger.on('click', function() {
-    var that = this;
-    getSelectType.call(that, 'oneArg', 'twoArg');
-  });
-
-  function getSelectType(button, arg1, arg2) {
-    if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'single') {
-      var that = this;
-      var clickableElements = $('.dropdown-menu li a');
-      clickableElements.on('click', function() {
-        that.innerHTML = this.innerHTML;
-      });
-    } else if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'multiple') {
-      var that = this;
-      var clickableElements = $('.dropdown-menu li input');
-      var valueArray = [];
-      clickableElements.on('click', function() {
-        console.log("values are", this.getAttribute('value'));
-        syncItems();
-      });
-      function syncItems() {
-        var stuff = clickableElements.toArray();
-        stuff.forEach(function(el, index, array) {
-          if (clickableElements.is(':checked')) {
-            var values = clickableElements.val();
-            console.log('values are', clickableElements.val());
-            var stuffVal = valueArray.push(values);
-            console.log("yep");
-          } else {
-          }
-        });
+    function floatLabelHandler() {
+      if (!this.value || this.value === "") {
+        $(this).removeClass(showClass);
+        $(this).prev().removeClass(onClass);
+      } else {
+        $(this).addClass(showClass);
+        $(this).prev().addClass(onClass);
       }
-    } else {
-      return;
     }
-  }
-})();
+  }());
 
-// FORM GROUP BUTTON CHANGE
-(function formBtnGroupChange() {
-  var channelBtn = $('.dice-btn-group button');
+  // Multi Select and Single Select Dropdown
+  (function selectDD() {
+    var toggleTrigger = $('.dropdown-toggle');
 
-  channelBtn.on('click', function(e) {
-    that = $(this);
-    e.preventDefault();
-    if (that.hasClass('inactive')) {
-      channelBtn.removeClass('active').addClass('inactive');
-      that.removeClass('inactive').addClass('active');
-    } else {
-      return;
+    toggleTrigger.on('click', function() {
+      var that = this;
+      getSelectType.call(that, 'oneArg', 'twoArg');
+    });
+
+    function getSelectType(button, arg1, arg2) {
+      if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'single') {
+        var that = this;
+        var clickableElements = $('.dropdown-menu li a');
+        clickableElements.on('click', function() {
+          that.innerHTML = this.innerHTML;
+        });
+      } else if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'multiple') {
+        var that = this;
+        var clickableElements = $('.dropdown-menu li input');
+        var valueArray = [];
+        clickableElements.on('click', function() {
+          console.log("values are", this.getAttribute('value'));
+          syncItems();
+        });
+        function syncItems() {
+          var stuff = clickableElements.toArray();
+          stuff.forEach(function(el, index, array) {
+            if (clickableElements.is(':checked')) {
+              var values = clickableElements.val();
+              console.log('values are', clickableElements.val());
+              var stuffVal = valueArray.push(values);
+              console.log("yep");
+            } else {
+            }
+          });
+        }
+      } else {
+        return;
+      }
     }
-  });
+  }());
 
-})();
+  // FORM GROUP BUTTON CHANGE
+  (function formBtnGroupChange() {
+    var channelBtn = $('.dice-btn-group button');
 
-// CUSTOM CHECKBOX CHECKS
-(function checkboxCheck() {
-  var checkboxes = $('.dice-checkbox');
-  var realChecks = $('.bootstrap-checkbox');
+    channelBtn.on('click', function(e) {
+      that = $(this);
+      e.preventDefault();
+      if (that.hasClass('inactive')) {
+        channelBtn.removeClass('active').addClass('inactive');
+        that.removeClass('inactive').addClass('active');
+      } else {
+        return;
+      }
+    });
 
-  checkboxes.on('click', function(e) {
-    srcEl = $(e.target);
-    if (srcEl.hasClass('icon-check-1')) {
-      srcEl.removeClass('icon-check-1');
-      srcEl.prev().removeAttr('checked');
-    } else {
-      srcEl.addClass('icon-check-1');
-      srcEl.prev().attr('checked', true);
-    }
-  });
-})();
+  }());
+
+  // CUSTOM CHECKBOX CHECKS
+  (function checkboxCheck() {
+    var checkboxes = $('.dice-checkbox');
+    var realChecks = $('.bootstrap-checkbox');
+
+    checkboxes.on('click', function(e) {
+      srcEl = $(e.target);
+      if (srcEl.hasClass('icon-check-1')) {
+        srcEl.removeClass('icon-check-1');
+        srcEl.prev().removeAttr('checked');
+      } else {
+        srcEl.addClass('icon-check-1');
+        srcEl.prev().attr('checked', true);
+      }
+    });
+  }());
 
 });
