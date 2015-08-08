22f828d492dc91bf57ca3f45374bbbe11be96f28
Ben Fortune <ben.fortune@gmail.com>
2015-08-07 23:02:41 -0500

    Work for adding checkbox input

--- js/index.js
+++ js/index.js
@@ -83,82 +83,87 @@ $(document).ready(function() {
 
   // FORM GROUP BUTTON CHANGE
     (function formBtnGroupChange() {
-    var channelBtn = $('.dice-btn-group button');
+        var channelBtn = $('.dice-btn-group button');
 
-    channelBtn.on('click', function(e) {
-      var that = $(this);
-      e.preventDefault();
-      if (that.hasClass('inactive')) {
-        channelBtn.removeClass('active').addClass('inactive');
-        that.removeClass('inactive').addClass('active');
-      }
-    });
-
+        channelBtn.on('click', function(e) {
+          var that = $(this);
+          e.preventDefault();
+          if (that.hasClass('inactive')) {
+            channelBtn.removeClass('active').addClass('inactive');
+            that.removeClass('inactive').addClass('active');
+          }
+        });
     }());
 
     // CUSTOM CHECKBOX CHECKS
     (function checkboxCheck() {
-    var checkboxes = $('.dice-checkbox');
-    var realChecks = $('.bootstrap-checkbox');
+        var checkboxes = $('.dice-checkbox');
+        var realChecks = $('.bootstrap-checkbox');
 
-    checkboxes.on('click', function(e) {
-      srcEl = $(e.target);
-      if (srcEl.hasClass('icon-check-1')) {
-        srcEl.removeClass('icon-check-1');
-        srcEl.prev().removeAttr('checked');
-      } else {
-        srcEl.addClass('icon-check-1');
-        srcEl.prev().attr('checked', true);
-      }
-    });
+        checkboxes.on('click', function(e) {
+          srcEl = $(e.target);
+          if (srcEl.hasClass('icon-check-1')) {
+            srcEl.removeClass('icon-check-1');
+            srcEl.prev().removeAttr('checked');
+          } else {
+            srcEl.addClass('icon-check-1');
+            srcEl.prev().attr('checked', true);
+          }
+        });
     }());
 
     // REMOVE/HIDE TAGS
     (function removeTags() {
-    var tags = $('.dice-tag');
-    tags.on('click', function(e) {
-      srcEl = $(e.target);
-      if ($(this).hasClass('add')) {
-          return;
-      } else {
-        srcEl.parent().addClass('remove');
-        addNewTag($(this).parent());
-      }
-    });
+        var tags = $('.dice-tag');
+        tags.on('click', function(e) {
+          srcEl = $(e.target);
+          if ($(this).hasClass('add')) {
+              return;
+          } else {
+            srcEl.parent().addClass('remove');
+            addNewTag($(this).parent());
+          }
+        });
     }());
 
     // ADD TAGS
     (function addTags() {
-    var tags = $('.dice-tag.add');
-    var inputs = $('.dice-tag.add input');
-    tags.on('click', function() {
-      $(this).find('.remove').removeClass('remove').focus();
-    });
-    inputs.on('blur', function() {
-      var value = $(this).val();
-      if (value) {
-        var parentEl = $(this).parent().parent();
-        $(this).parent().text(value).removeClass('add').append('<span>X</span>');
-        addNewTag(parentEl);
-        addTags();
-      } else {
-        return;
-      }
-    });
+        var tags = $('.dice-tag.add');
+        var inputs = $('.dice-tag.add input');
+        tags.on('click', function() {
+          $(this).find('.remove').removeClass('remove').focus();
+        });
+        inputs.on('blur', function() {
+          var value = $(this).val();
+          if (value) {
+            var parentEl = $(this).parent().parent();
+            $(this).parent().text(value).removeClass('add').append('<span>X</span>');
+            addNewTag(parentEl);
+            addTags();
+          } else {
+            return;
+          }
+        });
     }());
 
     // ADDING NEW TAGS
     function addNewTag(tagInput) {
-    var targetEl = $(tagInput).children().last();
-    if ($(targetEl).hasClass('add')) {
-      return;
-    } else {
-      var addTagMarkup = '<div class="dice-tag add"><span>+</span>Add Tag <input class="remove" type="text" placeholder="Add Tag"></div>'
-      $(tagInput).append(addTagMarkup);
-    }
+        var targetEl = $(tagInput).children().last();
+        if ($(targetEl).hasClass('add')) {
+          return;
+        } else {
+          var addTagMarkup = '<div class="dice-tag add"><span>+</span>Add Tag <input class="remove" type="text" placeholder="Add Tag"></div>'
+          $(tagInput).append(addTagMarkup);
+        }
     };
-    (function addInputs() {
-        
+    
+    (function addNewInput() {
+        var inputGroup = $('.dice-input-group');
+        var addBtn = $('.dice-input-add');
+        addBtn.on('click', function() {
+            inputGroup.append('<label for="checkbox1" class="checkbox-container"><input class="bootstrap-checkbox" type="checkbox" checked><span class="dice-checkbox icon-check-1"></span>Check me</label>');
+            checkboxCheck();
+        });
     }());
     
 
