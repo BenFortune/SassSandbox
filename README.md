f37bbeb0c8dfe0cca213f8b8b631e23332cc47ba
Ben Fortune <ben.fortune@gmail.com>
2015-11-27 09:48:05 -0600

    Misc cleanup
    - Remove toggle plugin
    - Syntax cleanup
    - Add to do list to readme

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 *[Bootstrap front-end framework](https://github.com/twbs)
 
 *[Bootstrap Slider](https://github.com/seiyria/bootstrap-slider)
+
+####TO DO
+- Get multi-select dropdown working
+- Fix issue with all tags removing when clicking tag text
+- Rebuild with vanilla javascript and remove jQuery dependency
--- js/index.js
+++ js/index.js
@@ -1,180 +1,182 @@
 $(document).ready(function() {
 
-  var mySlider = $("#slider").slider();
-      mySlider.slider('setValue', 8);
+	var mySlider = $("#slider").slider();
+		mySlider.slider('setValue', 8);
 
-  // Float Label
-  (function floatLabel() {
-    var floatInput = $('.float-label input');
-    var onClass = "float-label-on";
-    var showClass = "float-label";
+	// Float Label
+	(function floatLabel() {
+		var floatInput = $('.float-label input');
+		var onClass = "float-label-on";
+		var showClass = "float-label";
 
-    floatInput.on('focus', function(e) {
-      floatLabelHandler.call(e.target, 'something else');
-    });
-    floatInput.on('blur', function(e) {
-      floatLabelHandler.call(e.target, 'something else');
-    });
-    floatInput.on('keyup', function(e) {
-      floatLabelHandler.call(e.target, 'something else');
-    });
+		floatInput.on('focus', function(e) {
+			floatLabelHandler.call(e.target, 'something else');
+		});
+		floatInput.on('blur', function(e) {
+			floatLabelHandler.call(e.target, 'something else');
+		});
+		floatInput.on('keyup', function(e) {
+			floatLabelHandler.call(e.target, 'something else');
+		});
 
-    function floatLabelHandler() {
-      if (!this.value || this.value === "") {
-        $(this).removeClass(showClass);
-        $(this).prev().removeClass(onClass);
-      } else {
-        $(this).addClass(showClass);
-        $(this).prev().addClass(onClass);
-      }
-    }
-  }());
+		function floatLabelHandler() {
+			if (!this.value || this.value === "") {
+				$(this).removeClass(showClass);
+				$(this).prev().removeClass(onClass);
+			} else {
+				$(this).addClass(showClass);
+				$(this).prev().addClass(onClass);
+			}
+		}
+	}());
 
-  // Multi Select and Single Select Dropdown
-  (function selectDD() {
-    var toggleTrigger = $('.dropdown-toggle');
+	// Multi Select and Single Select Dropdown
+	(function selectDD() {
+		var toggleTrigger = $('.dropdown-toggle');
 
-    toggleTrigger.on('click', function() {
-      var that = this;
-      getSelectType.call(that, 'oneArg', 'twoArg');
-    });
+		toggleTrigger.on('click', function() {
+			var that = this;
+			getSelectType.call(that, 'oneArg', 'twoArg');
+		});
 
-    function getSelectType(button, arg1, arg2) {
-      if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'single') {
-        var that = this;
-        var clickableElements = $('.dropdown-menu li a');
+		function getSelectType(button, arg1, arg2) {
+			if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'single') {
+				var that = this;
+				var clickableElements = $('.dropdown-menu li a');
 
-        if ($(this).hasClass('dropdown-caret')) {
-          var btnText = $(that).prev();
-          clickableElements.on('click', function() {
-            btnText.text(this.innerHTML);
-          });
-        } else {
-          clickableElements.on('click', function() {
-            that.innerHTML = this.innerHTML;
-          });
-        }
-      } else if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'multiple') {
-        // TODO Finish Multi Select Work
-        var that = this;
-        var clickableElements = $('.dropdown-menu li input');
-        clickableElements.on('click', function() {
-          console.log("values are", this.getAttribute('value'));
-          if (valueArray === undefined) {
-            var valueArray = [];
-            var value = this.getAttribute('value');
-            var values = valueArray.push(value);
-            var allValues = valueArray;
-            $('#multipleBtn').text(valueArray);
-            console.log('all values', allValues);
-          } else {
-            console.log('all values where defined', allValues);
-            var value = this.getAttribute('value');
-            var values = allValues.push(' ' + value);
-            $('#multipleBtn').text(valueArray);
-          }
-        });
+				if ($(this).hasClass('dropdown-caret')) {
+					var btnText = $(that).prev();
+					clickableElements.on('click', function() {
+						btnText.text(this.innerHTML);
+					});
+				} else {
+					clickableElements.on('click', function() {
+						that.innerHTML = this.innerHTML;
+					});
+				}
+			} else if (this.hasAttribute('data-select-type') && this.getAttribute('data-select-type') === 'multiple') {
 
-        // TODO resuable function for multiple values
+				// TODO Finish Multi Select Work
+				
+				var that = this;
+				var clickableElements = $('.dropdown-menu li input');
+				clickableElements.on('click', function() {
+					console.log("values are", this.getAttribute('value'));
+					if (valueArray === undefined) {
+						var valueArray = [];
+						var value = this.getAttribute('value');
+						var values = valueArray.push(value);
+						var allValues = valueArray;
+						$('#multipleBtn').text(valueArray);
+						console.log('all values', allValues);
+					} else {
+						console.log('all values where defined', allValues);
+						var value = this.getAttribute('value');
+						var values = allValues.push(' ' + value);
+						$('#multipleBtn').text(valueArray);
+					}
+				});
 
-      }
-    }
-  }());
+				// TODO resuable function for multiple values
 
-  // FORM GROUP BUTTON CHANGE
-    (function formBtnGroupChange() {
-        var channelBtn = $('.dice-btn-group-tab button');
+			}
+		}
+	}());
 
-        channelBtn.on('click', function(e) {
-          var that = $(this);
-          e.preventDefault();
-          if (that.hasClass('inactive')) {
-            channelBtn.removeClass('active').addClass('inactive');
-            that.removeClass('inactive').addClass('active');
-          }
-        });
-    }());
+	// FORM GROUP BUTTON CHANGE
+	(function formBtnGroupChange() {
+		var channelBtn = $('.dice-btn-group-tab button');
 
-    // CUSTOM CHECKBOX CHECKS
-    (function checkboxCheck() {
-        var checkboxes = $('.dice-checkbox');
-        var realChecks = $('.bootstrap-checkbox');
+		channelBtn.on('click', function(e) {
+			var that = $(this);
+			e.preventDefault();
+			if (that.hasClass('inactive')) {
+				channelBtn.removeClass('active').addClass('inactive');
+				that.removeClass('inactive').addClass('active');
+			}
+		});
+	}());
 
-        checkboxes.on('click', function(e) {
-          srcEl = $(e.target);
-          if (srcEl.hasClass('icon-check-1')) {
-            srcEl.removeClass('icon-check-1');
-            srcEl.prev().removeAttr('checked');
-          } else {
-            srcEl.addClass('icon-check-1');
-            srcEl.prev().attr('checked', true);
-          }
-        });
-    }());
+	// CUSTOM CHECKBOX CHECKS
+	(function checkboxCheck() {
+		var checkboxes = $('.dice-checkbox');
+		var realChecks = $('.bootstrap-checkbox');
 
-    // REMOVE/HIDE TAGS
-    (function removeTags() {
-        var tags = $('.dice-tag');
-        tags.on('click', function(e) {
-          srcEl = $(e.target);
-          if ($(this).hasClass('add')) {
-              return;
-          } else {
-            srcEl.parent().addClass('remove');
-            addNewTag($(this).parent());
-          }
-        });
-    }());
+		checkboxes.on('click', function(e) {
+			srcEl = $(e.target);
+			if (srcEl.hasClass('icon-check-1')) {
+				srcEl.removeClass('icon-check-1');
+				srcEl.prev().removeAttr('checked');
+			} else {
+				srcEl.addClass('icon-check-1');
+				srcEl.prev().attr('checked', true);
+			}
+		});
+	}());
 
-    // ADD TAGS
-    (function addTags() {
-        var tags = $('.dice-tag.add');
-        var inputs = $('.dice-tag.add input');
-        tags.on('click', function() {
-          $(this).find('.remove').removeClass('remove').focus();
-        });
-        inputs.on('blur', function() {
-          var value = $(this).val();
-          if (value) {
-            var parentEl = $(this).parent().parent();
-            $(this).parent().text(value).removeClass('add').append('<span>X</span>');
-            addNewTag(parentEl);
-            addTags();
-          } else {
-            return;
-          }
-        });
-    }());
+	// REMOVE/HIDE TAGS
+	(function removeTags() {
+		var tags = $('.dice-tag');
+		tags.on('click', function(e) {
+			srcEl = $(e.target);
+			if ($(this).hasClass('add')) {
+				return;
+			} else {
+				srcEl.parent().addClass('remove');
+				addNewTag($(this).parent());
+			}
+		});
+	}());
 
-    // ADDING NEW TAGS
-    function addNewTag(tagInput) {
-        var targetEl = $(tagInput).children().last();
-        if ($(targetEl).hasClass('add')) {
-          return;
-        } else {
-          var addTagMarkup = '<div class="dice-tag add"><span>+</span>Add Tag <input class="remove" type="text" placeholder="Add Tag"></div>'
-          $(tagInput).append(addTagMarkup);
-        }
-    };
+	// ADD TAGS
+	(function addTags() {
+		var tags = $('.dice-tag.add');
+		var inputs = $('.dice-tag.add input');
+		tags.on('click', function() {
+			$(this).find('.remove').removeClass('remove').focus();
+		});
+		inputs.on('blur', function() {
+			var value = $(this).val();
+			if (value) {
+				var parentEl = $(this).parent().parent();
+				$(this).parent().text(value).removeClass('add').append('<span>X</span>');
+				addNewTag(parentEl);
+				addTags();
+			} else {
+				return;
+			}
+		});
+	}());
 
-    (function addNewInput() {
-        var inputGroup = $('.dice-input-group');
-        var addBtn = $('.dice-input-add');
-        addBtn.on('click', function() {
-            inputGroup.append('<label for="checkbox1" class="checkbox-container"><input class="bootstrap-checkbox" type="checkbox" checked><span class="dice-checkbox icon-check-1"></span>Check me</label>');
-            checkboxCheck();
-        });
-    }());
+	// ADDING NEW TAGS
+	function addNewTag(tagInput) {
+		var targetEl = $(tagInput).children().last();
+		if ($(targetEl).hasClass('add')) {
+			return;
+		} else {
+			var addTagMarkup = '<div class="dice-tag add"><span>+</span>Add Tag <input class="remove" type="text" placeholder="Add Tag"></div>'
+			$(tagInput).append(addTagMarkup);
+		}
+	};
 
-    (function diceToggle() {
+	(function addNewInput() {
+		var inputGroup = $('.dice-input-group');
+		var addBtn = $('.dice-input-add');
+		addBtn.on('click', function() {
+			inputGroup.append('<label for="checkbox1" class="checkbox-container"><input class="bootstrap-checkbox" type="checkbox" checked><span class="dice-checkbox icon-check-1"></span>Check me</label>');
+			checkboxCheck();
+		});
+	}());
+
+	(function diceToggle() {
 		// Initialize default status
-    	var diceToggle = $('.dice-toggle-control');
+		var diceToggle = $('.dice-toggle-control');
 		var diceToggleLabel = $('.dice-toggle-label');
 		diceToggle.addClass('on');
 
 		// Click event
 		diceToggle.on('click', function() {
-	 		if ($(this).hasClass('on')) {
+			if ($(this).hasClass('on')) {
 				$(this).removeClass('on').addClass('off');
 				diceToggleLabel.text('Off');
 			} else {
@@ -182,7 +184,7 @@ $(document).ready(function() {
 				diceToggleLabel.text('On');
 			}
 		})
-    }());
+	}());
 
 	// Initialize tooltips
 	$(function () {
